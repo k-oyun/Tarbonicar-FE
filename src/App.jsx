8058b8a217ee98777d8c5294fb436cd6dc2a28e0
rhkr8521 <rhkr8521@gmail.com>
2025-06-17 17:35:27 +0900

    [FEAT] 게시글 작성 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/header";
+import Header from "./components/Header.jsx";
 import ModalSample from "./pages/ModalSample.jsx";
+import ArticleWrite from "./pages/ArticleWrite.jsx";
 
 function App() {
-  return (
-    // <Header />
-    // <ModalSample/>
-    <BrowserRouter>
-      <Route></Route>
-    </BrowserRouter>
-  );
+    return (
+        // <Header />
+        // <ModalSample/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/article-write" element={<ArticleWrite />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
