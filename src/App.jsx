f77b486bf0132901dc0983c66743af96bd16ba3d
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-06-17 10:00:16 +0900

    [FEAT] : Main 페이지 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header";
 import ModalSample from "./pages/ModalSample.jsx";
+import Main from "./pages/Main.jsx";
 
 function App() {
   return (
     // <Header />
     // <ModalSample/>
     <BrowserRouter>
-      <Route></Route>
+      <Routes>
+        <Route Component={Main} path="/" />
+      </Routes>
     </BrowserRouter>
   );
 }
