f67a2465b131a5ecea451b750fea6d7261febbe7
thdwldmszz <tiffany_song@naver.com>
2025-06-18 14:38:36 +0900

    작업중인 마이페이지

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header";
 import ModalSample from "./pages/ModalSample.jsx";
+import MyPage from "./pages/MyPage.jsx";
 
 function App() {
   return (
     // <Header />
     // <ModalSample/>
     <BrowserRouter>
-      <Route></Route>
+      <Header />
+      <Routes>
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
