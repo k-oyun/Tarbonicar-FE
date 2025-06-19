55f982417f9760eed450dbb7b614dec0c00d7ce0
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-06-19 15:10:14 +0900

    [FEAT] : 메인화면 구현

---
+++ src/components/SelectBox.jsx
@@ -0,0 +1,119 @@
+import React, { useState, useRef, useEffect } from "react";
+import styled from "styled-components";
+
+const SelectBoxContainer = styled.div`
+  display: flex;
+  margin-top: 10px;
+  position: relative;
+  font-size: 13px;
+`;
+const SelectBoxButton = styled.button`
+  width: 110px;
+  height: 45px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  background-color: transparent;
+  border: ${(props) =>
+    props.$isSelected === "" ? "1px solid #d9d9d9" : "1px solid #002c5f"};
+  border-radius: 10px;
+  color: ${(props) => (props.$isSelected === "" ? "#d9d9d9" : "#002c5f")};
+  text-align: start;
+  cursor: pointer;
+  padding-left: 20px;
+  font-size: 13px;
+`;
+
+const SelectBoxList = styled.div`
+  width: 110px;
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: white;
+  position: absolute;
+  top: 37px;
+  border-bottom: ${(props) =>
+    !props.$isSelected ? "1px solid #d9d9d9" : "1px solid #002c5f"};
+  border-left: ${(props) =>
+    !props.$isSelected ? "1px solid #d9d9d9" : "1px solid #002c5f"};
+  border-right: ${(props) =>
+    !props.$isSelected ? "1px solid #d9d9d9" : "1px solid #002c5f"};
+  box-sizing: border-box;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+`;
+
+const SelectBoxItem = styled.li`
+  cursor: pointer;
+  width: 100%;
+  padding-left: 14px;
+  padding-top: 7px;
+  padding-bottom: 7px;
+  color: #d9d9d9;
+  list-style: none;
+  &:hover {
+    color: #002c5f;
+  }
+`;
+
+const SelectBox = ({
+  options,
+  placeholder,
+  value,
+  onSelect,
+  isSelected,
+  onClick,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const selectBoxRef = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        selectBoxRef.current &&
+        !selectBoxRef.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const selectedLabel = options.find((opt) => opt.value === value)?.label;
+
+  return (
+    <SelectBoxContainer ref={selectBoxRef}>
+      <SelectBoxButton
+        onClick={(e) => {
+          setIsOpen((prev) => !prev);
+          onClick?.(e);
+        }}
+        $isSelected={isSelected}
+      >
+        {selectedLabel || placeholder}
+      </SelectBoxButton>
+      {isOpen ? (
+        <SelectBoxList $isOpen={isOpen} $isSelected={isSelected}>
+          {options.map((opt) => (
+            <SelectBoxItem
+              key={opt.value}
+              onClick={() => {
+                onSelect(opt.value);
+                setIsOpen(false);
+              }}
+            >
+              {opt.label}
+            </SelectBoxItem>
+          ))}
+        </SelectBoxList>
+      ) : null}
+    </SelectBoxContainer>
+  );
+};
+
+export default SelectBox;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,15 +2,17 @@ import styled from "styled-components";
 import video1 from "../assets/videos/video1.mp4";
 import downArrow from "../assets/imgs/downArrow.png";
 import Car from "../assets/imgs/car.png";
-import { floatUpDown, fadeDown, moveDown } from "../styles/animation";
-import { useRef, useState } from "react";
+import { floatUpDown, fadeDown } from "../styles/animation";
+import { useEffect, useRef, useState } from "react";
+import SelectBox from "../components/SelectBox";
+import Header from "../components/header";
 
 const MainContainer = styled.div`
   width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  height: 100vh;
+  overflow-y: scroll;
   scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
 `;
 
 const VideoContainer = styled.div`
@@ -20,12 +22,13 @@ const VideoContainer = styled.div`
   justify-content: center;
   align-items: center;
   position: relative;
+  scroll-snap-align: start;
 `;
 
 const Video = styled.video`
   width: 100%;
   height: 100vh;
-  object-fit: contain;
+  object-fit: cover;
 `;
 
 const DownArrowIcon = styled.img`
@@ -44,6 +47,7 @@ const ReviewContainer = styled.div`
   align-items: center;
   width: 100%;
   height: 100vh;
+  scroll-snap-align: start;
 `;
 
 const CarImg = styled.img`
@@ -72,7 +76,9 @@ const SubText = styled.span`
 const DescContainer = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
   width: 470px;
+  height: 80px;
   font-size: 16px;
   color: #002c5f;
   font-weight: 400;
@@ -81,25 +87,21 @@ const DescContainer = styled.div`
 `;
 
 const SelectBoxContainer = styled.div`
-  display: flex;
+  display: ${(props) => (props.$isBtnPressed ? "flex" : "none")};
   width: 380px;
+  height: ${(props) => (props.$isBtnVisible ? "70px" : "220px")};
   margin-top: 10px;
   justify-content: space-between;
   animation: ${fadeDown} 0.3s ease-in-out;
 `;
 
-const SelectBox = styled.select`
-  width: 110px;
-  height: 45px;
-  border: 1px solid #d9d9d9;
-  border-radius: 10px;
-  color: #d9d9d9;
-  text-align: start;
-`;
-
 const WatchReviewBtn = styled.button`
+  display: ${(props) => (props.$isBtnVisible === false ? "none" : "flex")};
   width: 150px;
   height: 43px;
+  justify-content: center;
+  align-items: center;
+  border: none;
   border-radius: 10px;
   background-color: #002c5f;
   margin-top: 20px;
@@ -109,50 +111,144 @@ const WatchReviewBtn = styled.button`
 const Main = () => {
   const reviewRef = useRef(null);
   const [isBtnPressed, setIsBtnPressed] = useState(false);
+  const [isBtnVisible, setIsBtnVisible] = useState(true);
+  const [selectedType, setSelectedType] = useState("");
+  const [selectedModel, setSelectedModel] = useState("");
+  const [selectedYear, setSelectedYear] = useState("");
+  const [isReviewVisible, setIsReviewVisible] = useState(false);
+  const carType = [
+    { label: "전체보기", value: "all" },
+    { label: "승용차", value: "sedan" },
+    { label: "SUV", value: "suv" },
+    { label: "EV", value: "ev" },
+    { label: "승합차", value: "van" },
+  ];
+  const carModel = [
+    { label: "전체보기", value: "all" },
+    { label: "아반떼", value: "avante" },
+    { label: "소나타", value: "sonata" },
+    { label: "그랜저", value: "granduer" },
+    { label: "코나", value: "kona" },
+  ];
+  const carYear = [
+    { label: "전체보기", value: "all" },
+    { label: "2016", value: 2016 },
+    { label: "2017", value: 2017 },
+    { label: "2018", value: 2018 },
+    { label: "2019", value: 2019 },
+  ];
   const handleDownBtn = () => {
     reviewRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   const onClickReviewBtn = () => {
     setIsBtnPressed(true);
+    setIsBtnVisible(false);
   };
+
+  useEffect(() => {
+    if (selectedType && selectedModel && selectedYear !== "") {
+      setIsBtnVisible(true);
+    }
+    console.log(selectedType, selectedModel, selectedYear);
+  }, [selectedType, selectedModel, selectedYear]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsReviewVisible(entry.isIntersecting);
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    if (reviewRef.current) {
+      observer.observe(reviewRef.current);
+    }
+
+    return () => {
+      if (reviewRef.current) observer.unobserve(reviewRef.current);
+    };
+  }, []);
+
   return (
-    <MainContainer>
-      <VideoContainer>
-        <Video src={video1} autoPlay muted loop playsInline controls={false} />
-        <DownArrowIcon src={downArrow} onClick={handleDownBtn} />
-      </VideoContainer>
-      <ReviewContainer ref={reviewRef}>
-        <CarImg src={Car} />
-        <TextContainer $mt="30px">
-          <LogoText>타보니까</LogoText>
-        </TextContainer>
-        <TextContainer $mt="15px">
-          <SubText>현대자통차에 특화된 프리미엄 차량 커뮤니티 플랫폼</SubText>
-        </TextContainer>
-        <DescContainer>
-          실제 운전자들의 시승 후기, 차량 리뷰, 차량 팁 등 신뢰도 높은 정보를
-          <br /> 한곳에서 확인하고, 나와 잘 맞는 차량을 찾는 데 도움을
-          받아보세요
-        </DescContainer>
-        {isBtnPressed ? (
-          <SelectBoxContainer>
-            <SelectBox>
-              <option>차종</option>
-            </SelectBox>
-            <SelectBox>
-              <option>차량</option>
-            </SelectBox>
-            <SelectBox>
-              <option>연식</option>
-            </SelectBox>
+    <>
+      <Header isReviewVisible={isReviewVisible} />
+      <MainContainer>
+        <VideoContainer>
+          <Video
+            src={video1}
+            autoPlay
+            muted
+            loop
+            playsInline
+            controls={false}
+          />
+          <DownArrowIcon src={downArrow} onClick={handleDownBtn} />
+        </VideoContainer>
+        <ReviewContainer ref={reviewRef}>
+          <CarImg src={Car} />
+          <TextContainer $mt="30px">
+            <LogoText>타보니까</LogoText>
+          </TextContainer>
+          <TextContainer $mt="15px">
+            <SubText>현대자통차에 특화된 프리미엄 차량 커뮤니티 플랫폼</SubText>
+          </TextContainer>
+          <DescContainer>
+            실제 운전자들의 시승 후기, 차량 리뷰, 차량 팁 등 신뢰도 높은 정보를
+            <br /> 한곳에서 확인하고, 나와 잘 맞는 차량을 찾는 데 도움을
+            받아보세요
+          </DescContainer>
+
+          <SelectBoxContainer
+            $isBtnPressed={isBtnPressed}
+            $isBtnVisible={isBtnVisible}
+          >
+            <SelectBox
+              options={carType}
+              value={selectedType}
+              onSelect={(val) => setSelectedType(val)}
+              isSelected={selectedType}
+              placeholder="차종"
+              onClick={() => {
+                setIsBtnVisible(false);
+              }}
+            />
+            <SelectBox
+              options={carModel}
+              value={selectedModel}
+              onSelect={(val) => setSelectedModel(val)}
+              isSelected={selectedModel}
+              placeholder="차량"
+              onClick={() => {
+                setIsBtnVisible(false);
+              }}
+            />
+            <SelectBox
+              options={carYear}
+              value={selectedYear}
+              onSelect={(val) => {
+                console.log("부모가 받은 값", val);
+                setSelectedYear(val);
+              }}
+              isSelected={selectedYear}
+              placeholder="연식"
+              onClick={() => {
+                setIsBtnVisible(false);
+              }}
+            />
           </SelectBoxContainer>
-        ) : null}
-        <WatchReviewBtn onClick={onClickReviewBtn} $isBtnPressed={isBtnPressed}>
-          후기보기
-        </WatchReviewBtn>
-      </ReviewContainer>
-    </MainContainer>
+
+          <WatchReviewBtn
+            onClick={onClickReviewBtn}
+            $isBtnVisible={isBtnVisible}
+          >
+            후기보기
+          </WatchReviewBtn>
+        </ReviewContainer>
+      </MainContainer>
+    </>
   );
 };
 export default Main;
